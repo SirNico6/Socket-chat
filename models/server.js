f2f7ccd47f41c2a89a0ecc6d863866a0f59603ac
Nicolas Arevalo <gabrielnicolas512@outlook.com>
2023-03-17 17:05:41 -0300

    Refactored

--- models/server.js
+++ models/server.js
@@ -16,11 +16,7 @@ class Server {
 
         this.paths = {
             auth:       '/api/auth',
-            buscar:     '/api/buscar',
-            categorias: '/api/categorias',
-            productos:  '/api/productos',
-            usuarios:   '/api/usuarios',
-            uploads:    '/api/uploads',
+            usuarios:   '/api/usuarios'
         }
 
 
@@ -53,23 +49,12 @@ class Server {
         // Directorio Público
         this.app.use( express.static('public') );
 
-        // Fileupload - Carga de archivos
-        this.app.use( fileUpload({
-            useTempFiles : true,
-            tempFileDir : '/tmp/',
-            createParentPath: true
-        }));
-
     }
 
     routes() {
         
         this.app.use( this.paths.auth, require('../routes/auth'));
-        this.app.use( this.paths.buscar, require('../routes/buscar'));
-        this.app.use( this.paths.categorias, require('../routes/categorias'));
-        this.app.use( this.paths.productos, require('../routes/productos'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
-        this.app.use( this.paths.uploads, require('../routes/uploads'));
         
     }
 
